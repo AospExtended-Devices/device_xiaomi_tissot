6735b6520a78ade50f19f5c930275c2aa3892ac8
Dan Pasanen <dan.pasanen@gmail.com>
2017-12-03 00:49:32 -0600

    Add extra root folders

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -209,6 +209,9 @@ USE_SENSOR_MULTI_HAL := true
 include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/sepolicy
 
+# Root
+BOARD_ROOT_EXTRA_FOLDERS := dsp firmware persist
+
 # Wi-Fi
 BOARD_HAS_QCOM_WLAN := true
 BOARD_HAS_QCOM_WLAN_SDK := true
