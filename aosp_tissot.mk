9e17a2e9438e864a7bee9552519b6aae701d5378
Shubham Singh <coolsks94@gmail.com>
2018-03-19 15:42:00 +0530

    tissot: Aex bringup

--- aosp_tissot.mk
+++ aosp_tissot.mk
@@ -16,10 +16,10 @@
 
 $(call inherit-product, device/xiaomi/tissot/full_tissot.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AEX stuff.
+$(call inherit-product, vendor/aosp/common.mk)
 
-PRODUCT_NAME := lineage_tissot
+PRODUCT_NAME := aosp_tissot
 BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
