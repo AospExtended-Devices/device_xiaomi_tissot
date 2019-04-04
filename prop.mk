6b402012c81c316b6bd4a6d561107fd45fda8139
Artem Borisov <dedsa2002@gmail.com>
2019-04-04 22:16:49 +0530

    tissot: Move system props to makefile
    Change-Id: Id06b3650f249cdcd3cd80b296b108d459be58b02

---
+++ prop.mk
@@ -0,0 +1,8 @@
+# Camera
+PRODUCT_PROPERTY_OVERRIDES += \
+vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.lineageos.snap \
+persist.camera.dual.camera=0
+
+# Radio
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.radio.aosp_usr_pref_sel=true
