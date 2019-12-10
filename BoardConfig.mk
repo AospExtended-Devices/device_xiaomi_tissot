92db8df5e9044a2bd215d5a2af46a1846aa8367d
abhinavgupta371 <abhinavgupta371@gmail.com>
2019-12-10 19:29:04 +0530

    tissot: Ship prebuilt Twrp

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -45,7 +45,7 @@ VENDOR_SECURITY_PATCH := 2019-05-05
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
 # Twrp
--include device/twrp/tissot/twrp.mk
+-include $(DEVICE_PATH)/twrp/twrp.mk
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
