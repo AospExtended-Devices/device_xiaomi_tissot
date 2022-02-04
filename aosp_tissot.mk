b626fdf9901f7c99086c77c971344921d268ce1e
Mohammad Hasan Keramat J <ikeramat@protonmail.com>
2022-02-04 20:15:24 +0330

    tissot: Set bootanimation resolution
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
    Change-Id: I2f11811fe8619d228f2eb50f6d5d7a9ecb4d7f9b

--- aosp_tissot.mk
+++ aosp_tissot.mk
@@ -11,6 +11,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common AEX stuff.
 $(call inherit-product, vendor/aosp/common.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Inherit from tissot device
 $(call inherit-product, device/xiaomi/tissot/device.mk)
